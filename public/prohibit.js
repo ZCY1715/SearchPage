fa1b28fea2c82869bf92b9a6b7c39ef7eb75bcac
ZCY1715 <zcy1715614103@163.com>
2023-03-16 23:39:58 +0800

    config of vercel about serveless

--- public/prohibit.js
+++ public/prohibit.js
@@ -8,9 +8,9 @@ document.oncontextmenu = function () {
 
 // 禁止打开控制台与查看源代码
 document.onkeydown = function (e) {
-  // if (event.keyCode == 123) {
-  //   return false;
-  // }
+  if (event.keyCode == 123) {
+    return false;
+  }
   if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
     return false
   }
